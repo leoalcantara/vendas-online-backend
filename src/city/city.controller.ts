b439c16deca733ee270c82ac434ad889e6574a1a
unknown <leonidio.ti@gmail.com>
2023-08-20 21:59:49 -0300

    feature: search cities by state id

---
+++ src/city/city.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CityService } from './city.service';
+import { CityEntity } from './entities/city.entity';
+
+@Controller('city')
+export class CityController {
+    constructor(
+        private readonly cityService:CityService,
+    ){}
+
+    @Get('/:stateId')
+    async getAllCitesByStateId(
+        @Param('stateId') stateId: number
+    ): Promise<CityEntity[]>{
+        return this.cityService.getAllCityByStateId(4);
+    }
+
+}
